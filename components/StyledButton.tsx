2224afaa232f24f559efd689646a86ab6f13a330
Eggysgames <eggy55555@gmail.com>
2023-07-18 15:45:38 +0800

    Dynamic Buttons
    Dynamic Buttons with colour, hover, textsize attributes

--- components/StyledButton.tsx
+++ components/StyledButton.tsx
@@ -3,15 +3,20 @@ import Link, { LinkProps } from "next/link";
 type Props = {
   inserttext: string
   link:string
+  colour:string
+  hover:string
+  textsize:string
 }
 
-const StyledButton = ({ inserttext, link }: Props) => {
+const StyledButton = ({ inserttext, link, colour, hover, textsize }: Props) => {
   return (
-    <Link className="bg-teal-800/90 hover:bg-teal-950/90 text-white font-bold py-2 px-6 rounded-full border-none drop-shadow-lg text-2xl" href={link}>
+    <Link className={`${colour} ${hover} ${textsize} text-white font-bold py-2 px-6 rounded-full border-none drop-shadow-lg`} href={link}>
       {inserttext}
     </Link>
   );
 };
+
+
 
 export default StyledButton;
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,16 @@ import StyledButton from "../components/StyledButton";
 
 const Index = () => {
   return (
-    <div className="relative flex items-center justify-center h-screen">
+    <div className="relative flex items-center justify-center h-screen py-16">
+
 
     <section className='relative w-[700px] h-[700px]'>
+
+    
+      <div className='text-center -translate-y-6'>
+        <StyledButton inserttext="Betting and Cheating Guideline" link="/chessgame" colour='bg-teal-600/80' hover='hover:bg-teal-900' textsize='text-4xl'/>
+      </div>
+
 
       <Image
         src="/StaticChessboard.png"
@@ -15,10 +22,9 @@ const Index = () => {
       />
 
       <div className="flex flex-col items-center space-y-4 absolute inset-0 justify-center">
-        <StyledButton inserttext="Play for Money" link="/chessgame" />
-        <StyledButton inserttext="Play for Fun" link="/chessgame" />
+        <StyledButton inserttext="Play for Money" link="/chessgame" colour='bg-teal-600/90' hover='hover:bg-teal-900' textsize='text-2xl'/>
+        <StyledButton inserttext="Play for Fun" link="/chessgame" colour='bg-teal-600/90' hover='hover:bg-teal-900' textsize='text-2xl'/>
       </div>
-
     </section>
 
 
