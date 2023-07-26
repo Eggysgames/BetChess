16c124b5b89e1bb888907c146987a93e1129d186
Eggysgames <eggy55555@gmail.com>
2023-07-26 14:50:26 +0800

    Cheating Policy Added
    Cheating Policy Added v1

---
+++ pages/cheatingpolicy.tsx
@@ -0,0 +1,57 @@
+const cheatingpolicy = () => {
+    return (
+
+        <div className="my-10 mx-auto max-w-6xl px-32 py-8 bg-slate-800 rounded-lg shadow drop-shadow-xl shadow-slate-900 text-left">
+
+        <article className="prose prose-2xl text-white max-w-none">
+
+        <h1 className="text-white underline">Betting and Cheating Policy</h1>
+
+        <p>
+          Computers and computer-assisted players are not allowed to play. Please do not get assistance from chess engines, databases, or from 
+          other players while playing. Also note that making multiple accounts is strongly discouraged and excessive multi-accounting will lead
+          to being banned. You <b>must</b> follow these rules or else your entire <i>family bloodline</i> will be destroyed forever.
+        </p>
+
+        <p>
+          By registering, you agree to be bound by our Terms of Service. Additionally please read our Privacy Policy.
+        </p>
+
+        
+        <p>
+          if you are suspected of cheating:
+          <br /><br />
+          Your account is immediately frozen pending a thorough review.
+          <br />
+          If we conclude that cheating has occured:
+          <br /><br />
+          All winnings will be forfeited and distributed to impacted players.
+          <br /><br />
+          Your initial deposit will be refunded.
+          <br /><br />
+          You will be permanently banned from the site.
+          <br /><br />
+          Your name, as well as User ID, will be listed on the Banned Players Page.
+          <br /><br />
+          
+        </p>
+          <p>FAQ</p>
+        <blockquote className="text-left text-slate-400">
+          <p>Why was my account Banned?</p>
+        </blockquote>
+        <p>Because fuck you thats why.</p>
+        <blockquote className="text-left text-slate-400">
+          <p>Can I suck your dick to get my account back?</p>
+        </blockquote>
+        <p>No.</p>
+        <br /><br />
+
+        
+
+      </article>
+      
+      </div>
+    ); 
+  }
+
+  export default cheatingpolicy
