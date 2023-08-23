be84a172f3fc40de96b23691ca24404c3ab982ca
Eggysgames <eggy55555@gmail.com>
2023-08-23 16:41:14 +0800

    Update Testing Sign Out
    Update Testing Sign Out

--- pages/testing.js
+++ pages/testing.js
@@ -1,32 +1,38 @@
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 
-  import { useState, useEffect } from 'react'
-  import { createClient } from '@supabase/supabase-js'
-  import { Auth } from '@supabase/auth-ui-react'
-  import { ThemeSupa } from '@supabase/auth-ui-shared'
+const supabase = createClient('https://ttlaembyimpxjuovpmxk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0bGFlbWJ5aW1weGp1b3ZwbXhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI3NzA2NTcsImV4cCI6MjAwODM0NjY1N30.f6YXhReklfjQMe6sfVAqjyraiXgzjcH6W-2bOkCn_Sw')
 
-  const supabase = createClient('https://ttlaembyimpxjuovpmxk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0bGFlbWJ5aW1weGp1b3ZwbXhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI3NzA2NTcsImV4cCI6MjAwODM0NjY1N30.f6YXhReklfjQMe6sfVAqjyraiXgzjcH6W-2bOkCn_Sw')
+export default function App() {
+  const [session, setSession] = useState(null)
 
-  export default function App() {
-    const [session, setSession] = useState(null)
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
 
-    useEffect(() => {
-      supabase.auth.getSession().then(({ data: { session } }) => {
-        setSession(session)
-      })
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
 
-      const {
-        data: { subscription },
-      } = supabase.auth.onAuthStateChange((_event, session) => {
-        setSession(session)
-      })
+    return () => subscription.unsubscribe()
+  }, [])
 
-      return () => subscription.unsubscribe()
-    }, [])
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+  } else {
+    return (
+      <div>
+        <p className='text-white'>Welcome, {session.user.email}!</p>
+        <button onClick={() => supabase.auth.signOut()} className='text-white'>
+        Sign Out
+      </button>
+      </div>
 
-    if (!session) {
-      return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
-    }
-    else {
-      return (<div>Logged in!</div>)
-    }
+    )
   }
+}
