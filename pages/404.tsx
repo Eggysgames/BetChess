b23aae669b84fb99b47618769b97e982e32775e4
Eggysgames <eggy55555@gmail.com>
2023-11-21 15:55:34 +0800

    Retry 404

---
+++ pages/404.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import Topnav from "@/components/topnav";
+
+const NotFound = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center mt-32">
+        <Image
+          src="/brokenhorse.png"
+          alt="Broken Horse"
+          width={400}
+          height={400}
+        />
+      </div>
+
+      <div className="text-center text-white mt-16 mb-16">
+        Oops you hit a dead end :( perhaps you meant to go to the{" "}
+        <Link href="/" passHref>
+          <span className="text-purple-400 hover:text-purple-700 underline">
+            Homepage
+          </span>
+        </Link>
+        {""} instead?
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
