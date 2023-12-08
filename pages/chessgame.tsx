a6e01ebb35aa6f5bf71c65306f185f6e0daf831a
Eggysgames <eggy55555@gmail.com>
2023-12-08 11:55:26 +0800

    Chatbox UI
    Chatbox UI

--- pages/chessgame.tsx
+++ pages/chessgame.tsx
@@ -4,8 +4,21 @@ import { Chessboard } from "react-chessboard";
 
 export default function PlayRandomMoveEngine() {
   const [game, setGame] = useState(new Chess());
+  const [inputText, setInputText] = useState("");
+  const [conversation, setConversation] = useState<string[]>([]);
 
   let mover = false;
+
+  const handleInputChange = (event: any) => {
+    setInputText(event.target.value);
+  };
+
+  const handleEnterPress = (event: any) => {
+    if (event.key === "Enter") {
+      setConversation([...conversation, inputText]);
+      setInputText("");
+    }
+  };
 
   function makeAMove(move: any) {
     if (!game.isDraw()) {
@@ -68,7 +81,7 @@ export default function PlayRandomMoveEngine() {
   return (
     <div>
       <div className="flex justify-left items-center ml-[15%]">
-        <div className="flex flex-col items-center w-[780px] mt-28">
+        <div className="flex flex-col items-center w-[740px] mt-32">
           <Chessboard
             position={game.fen()}
             onPieceDrop={onDrop}
@@ -76,13 +89,22 @@ export default function PlayRandomMoveEngine() {
           />
         </div>
 
-        <div className="flex flex-col items-center justify-between shadow-md drop-shadow-xl rounded-3xl bg-slate-800 mb-5 h-[650px] w-[550px] ml-16 mt-32">
-          <div className=""></div>
-          <div className="">
+        <div className="shadow-md drop-shadow-xl rounded-3xl bg-slate-800 mb-5 h-[650px] w-[550px] ml-16 mt-32 flex flex-col">
+          <div className="m-4 flex-1 overflow-y-auto">
+            {conversation.map((message, index) => (
+              <p key={index} className="text-white text-left">
+                {message}
+              </p>
+            ))}
+          </div>
+          <div className="flex flex-col items-center justify-between mt-4">
             <input
-              className=" rounded-3xl bg-slate-600 p-2 mb-8 w-[500px]"
+              className="rounded-3xl bg-slate-600 p-2 mb-8 w-[500px] text-white"
               type="text"
               placeholder="Aa"
+              value={inputText}
+              onChange={handleInputChange}
+              onKeyUp={handleEnterPress}
             />
           </div>
         </div>
